43e4bdc031df78c2cd31d208898e029fa8bf0c83
kushwah-Anjali <anjalikushwah3333@gmail.com>
2025-10-13 05:45:41 -0800

    done brute force

--- countFrequency_Array_Element.java
+++ countFrequency_Array_Element.java
@@ -1,33 +1,29 @@
 package BasicHashing;
-import java.util.*;
 
 public class countFrequency_Array_Element {
     public static void main(String[] args) {
-        int nums[] = {16,1, 2, 2, 1, 3};
-        List<int[]> result = frequency(nums);
+        int nums[] = { 14, 1, 2, 2, 1, 3 };
 
-        // Print the result
-        for (int[] pair : result) {
-            System.out.println(Arrays.toString(pair));
-        }
+        int n = nums.length;
+       brute_Force(nums,n);
+
+
     }
 
-    static List<int[]> frequency(int arr[]) {
-        // Step 1: Count frequencies
-        // HashMap<Integer, Integer> map = new HashMap<>();
-Map<Integer, Integer> map = new LinkedHashMap<>();
-
-        for (int i = 0; i < arr.length; i++) {
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
-        }
-
-        // Step 2: Convert map into list of [key, value] pairs
-        List<int[]> output = new ArrayList<>();
-
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            output.add(new int[]{entry.getKey(), entry.getValue()});
-        }
-
-        return output;
+    static void brute_Force(int nums[],int n){
+         boolean[] visited = new boolean[n];
+      for(int i=0;i<n;i++){
+        int count=1;
+        if(visited[i])
+         continue;
+         for(int j=i+1;j<n;j++){
+            if(nums[j]==nums[i]){
+                visited[j]=true;
+                count++;
+            }
+         } System.out.println(nums[i] + " appears " + count + " times");
+      }
+      
     }
+
 }

16f52fc5d7623b541832575b07fe3d8bb173a8c7
kushwah-Anjali <anjalikushwah3333@gmail.com>
2025-10-15 07:00:29 +0530

    Added new files and updates

---
+++ highest_lowest_Frequency.java
@@ -0,0 +1,9 @@
+package BasicHashing;
+public class highest_lowest_Frequency {
+    public static void main(String args[]){
+        int arr[]={10,2,10,2,10,5};
+        for(int num:arr){
+            System.out.print(num+" ");
+        }
+    }
+}

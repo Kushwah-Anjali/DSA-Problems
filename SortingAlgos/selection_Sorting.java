709462de8badd8fc9e7c11ca5825ebf89082655e
kushwah-Anjali <Kushwah-Anjali>
2025-10-23 06:35:14 +0530

    chages made in selection sort

--- SortingAlgos/selection_Sorting.java
+++ SortingAlgos/selection_Sorting.java
@@ -3,7 +3,7 @@ package SortingAlgos;
 public class selection_Sorting {
     public static void main(String args[]) {
         int arr[] = { 4, 5, 1, 2, 3 };
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length-1; i++) {
             int smallest = i;
 
             for (int j = i + 1; j < arr.length; j++) {

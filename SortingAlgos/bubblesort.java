a8672e87220976c8dfb3e7be6fe476d7714d9b88
kushwah-Anjali <Kushwah-Anjali>
2025-10-25 05:23:06 +0530

    bubble sort done with best case

--- SortingAlgos/bubblesort.java
+++ SortingAlgos/bubblesort.java
@@ -2,20 +2,25 @@ package SortingAlgos;
 
 public class bubblesort {
     public static void main(String args[]) {
-        int arr[] = { 5, 3, 4, 1, 2 };
+        int arr[] = { 1, 2, 3, 5, 4 };
+        boolean swaped;
         for (int i = 0; i < arr.length - 1; i++) {
+            swaped = false;
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j + 1];
-                    arr[j + 1] = arr[j];s
+                    arr[j + 1] = arr[j];
                     arr[j] = temp;
+                    swaped = true;
                 }
-            }
 
+            }
+            if (swaped == false) {
+                break;
+            }
         }
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
     }
-
 }
